08a51a809d4c5db79d38f4f9186015d9b2a971d9
Luiz D C Cardoso <lzdavicc@gmail.com>
2023-11-02 22:02:09 -0300

    ADD page files added page files and linked the respective <a> components

--- assets/javascript/page_components.js
+++ assets/javascript/page_components.js
@@ -4,12 +4,12 @@ headerElement.innerHTML = (`
     <h2 class="header__title">Dev From Zer0</h2>
     <nav class="header__nav">
         <ul class="list">
-            <li class="list__item"><a href="#">Início</a></li>
-            <li class="list__item"><a href="#">Quem Somos</a></li>
-            <li class="list__item"><a href="#">Equipe</a></li>
+            <li class="list__item"><a href="index.html">Início</a></li>
+            <li class="list__item"><a href="quem_somos.html">Quem Somos</a></li>
+            <li class="list__item"><a href="#">-------</a></li>
         </ul>
     </nav>
-    <button><a href="#">Comprar</a></button>
+    <button><a href="checkout.html">Comprar</a></button>
 `)
 
 let footerElement = document.getElementById("footer")
@@ -22,9 +22,9 @@ footerElement.innerHTML = (`
         </ul>
         <ul class="footer__list">
             <h4 class="footer__list__title">Contato e FAQ</h4>
-            <li class="footer__list__item"><a href="">Suporte</a></li>
-            <li class="footer__list__item"><a href="">Vendas</a></li>
-            <li class="footer__list__item"><a href="">FAQ</a></li>
+            <li class="footer__list__item"><a href="suporte.html">Suporte</a></li>
+            <li class="footer__list__item"><a href="checkout.html">Vendas</a></li>
+            <li class="footer__list__item"><a href="faq.html">FAQ</a></li>
         </ul>
         <div class="footer__side-section">
             <img src="" alt="" class="footer__side-section__logo">
